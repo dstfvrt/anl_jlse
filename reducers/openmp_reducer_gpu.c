9ec8cd23c23aed00a51809b6641c37854aa10b84
dstfvrt <dstfvrt@gmail.com>
2021-06-14 11:27:46 -0500

    add runtime capture

--- reducers/openmp_reducer_gpu.c
+++ reducers/openmp_reducer_gpu.c
@@ -20,15 +20,45 @@ int main(int argc, char **argv) {
     int nBytes;
     nBytes = N*sizeof(unsigned);
 
+    /* Timing variables */
+    struct timeval etstart, etstop;
+    struct timezone tzdummy;
+    clock_t etstart2, etstop2;
+    unsigned long long usecstart, usecstop;
+    struct tms cputstart, cputstop;
+
     h = (unsigned *)malloc(nBytes);
     dense(h);
 
+    /* Start Clock */
+    printf("\nStarting clock.\n");
+    gettimeofday(&etstart, &tzdummy);
+    etstart2 = times(&cputstart);
+    
     #pragma omp target teams distribute parallel for
     for (unsigned i = 0; i < N; i++) {
         result += h[i];
     } 
 
-    printf("%d", result);
+    /* Stop Clock */
+    gettimeofday(&etstop, &tzdummy);
+    etstop2 = times(&cputstop);
+    printf("Stopped clock.\n");
+    usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
+    usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
+
+    printf("Checksum: %u\n", result);
+
+    /* Display timing results */
+    printf("\nElapsed time = %g ms.\n",
+           (float)(usecstop - usecstart)/(float)1000);
+
+    printf("(CPU times are accurate to the nearest %g ms)\n",
+           1.0/(float)CLOCKS_PER_SEC * 1000.0);
+    printf("My total CPU time for parent = %g ms.\n",
+           (float)( (cputstop.tms_utime + cputstop.tms_stime) -
+                    (cputstart.tms_utime + cputstart.tms_stime) ) /
+           (float)CLOCKS_PER_SEC * 1000);
 
     exit(0);
 }
