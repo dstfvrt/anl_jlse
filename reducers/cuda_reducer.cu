f6da9ab7a2d69f8fe47094754977b0c342f69f0f
dstfvrt <dstfvrt@gmail.com>
2021-06-15 02:12:57 -0500

    more reducers

--- reducers/cuda_reducer.cu
+++ reducers/cuda_reducer.cu
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <sys/time.h>
 #include <time.h>
+#include <omp.h>
 
-#define N 1000000
+
+#define N 1000
 
 /* Summary: reduction for 1mil unsigned values
  * by custom CUDA implementation.
- * Checksum: 383
+ * Checksum: 490204
  */
+
 __global__ void reduce_GPU(unsigned* d) {
     /* Shared memory */
     extern __shared__ unsigned sdata[];
 
     /* load into shared memory */
-    unsigned tid = threadIdx.x;
-    unsigned i = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
     sdata[tid] = d[i];
     __syncthreads();
 
@@ -42,17 +50,42 @@ int main(int argc, char **argv) {
 
     unsigned* h;
     unsigned* d;
-    unsigned result;
+    unsigned* result;
     unsigned nBytes;
     nBytes = N*sizeof(unsigned);
 
     h = (unsigned *)malloc(nBytes);
+    result = (unsigned *)malloc(nBytes);
     dense(h);
+
+    /* Timing variables */
+    struct timeval etstart, etstop;
+    struct timezone tzdummy;
+    clock_t etstart2, etstop2;
+    unsigned long long usecstart, usecstop;
+    struct tms cputstart, cputstop;
+
+    cudaMalloc(&d, nBytes);
+    cudaMemcpy(d, h, nBytes, cudaMemcpyHostToDevice);
+
+    /* Start Clock */
+    printf("\nStarting clock.\n");
+    gettimeofday(&etstart, &tzdummy);
+    etstart2 = times(&cputstart);
 
     reduce_GPU<<<(N+1023) / 1024, 1024, nBytes>>>(d);
 
-    cudaMemcpy(&result, d, nBytes, cudaMemcpyDeviceToHost);
-    printf("Checksum: %u\n", result);
+    /* Stop Clock */
+    gettimeofday(&etstop, &tzdummy);
+    etstop2 = times(&cputstop);
+    printf("Stopped clock.\n");
+    usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
+    usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
+    printf("\nElapsed time = %g ms.\n",
+           (float)(usecstop - usecstart)/(float)1000);
+
+    cudaMemcpy(result, d, nBytes, cudaMemcpyDeviceToHost);
+    printf("Checksum: %u\n", result[0]);
 
     exit(0);
 }
--- reducers/serial_reducer.c
+++ reducers/serial_reducer.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <math.h>
-#include <sys/types.h>
-#include <sys/times.h>
-#include <sys/time.h>
-#include <time.h>
 
-#define N 65536
+#define N 1000
 
 /*  Summary: serial reduction for 1mil unsigned values.
- *  Checksum: 499562283.
+ *  Checksum: 490204.
  */
 
 void dense(unsigned* h) {
@@ -32,33 +27,11 @@ int main(int argc, char **argv) {
     h = (unsigned *)malloc(nBytes);
     dense(h);
 
-    /* Timing variables */
-    struct timeval etstart, etstop;
-    struct timezone tzdummy;
-    clock_t etstart2, etstop2;
-    unsigned long long usecstart, usecstop;
-    struct tms cputstart, cputstop;
-
-    /* Start Clock */
-    printf("\nStarting clock.\n");
-    gettimeofday(&etstart, &tzdummy);
-    etstart2 = times(&cputstart);
-    
     for (unsigned i = 0; i < N; i++) {
         result += h[i];
     } 
 
-    /* Stop Clock */
-    gettimeofday(&etstop, &tzdummy);
-    etstop2 = times(&cputstop);
-    printf("Stopped clock.\n");
-    usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
-    usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
-
     printf("Checksum: %u\n", result);
-
-    printf("\nElapsed time = %g ms.\n",
-           (float)(usecstop - usecstart)/(float)1000);
 
     exit(0);
 }
